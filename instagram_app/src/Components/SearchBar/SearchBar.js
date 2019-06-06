379530b6d22b0666efc2c05496158308459d0231
Wes-White <wesleyrwhite86@gmail.com>
2019-06-05 20:49:13 -0700

    changed SearchBar to be responsive

--- instagram_app/src/Components/SearchBar/SearchBar.js
+++ instagram_app/src/Components/SearchBar/SearchBar.js
@@ -9,26 +9,29 @@ import FormControl from 'react-bootstrap/FormControl';
 function SearchBar(props) {
     return (  
         <div className='search-bar'>
-            <Navbar sticky='top' bg="light" variant="light">
+            <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#home"><img className='logo' src="https://img.icons8.com/plasticine/100/000000/instagram.png" alt='logo'></img></Navbar.Brand>
             <img className='writing' src={require('../../Images/instalogo.png' )}  alt='writingLogo'></img>
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-             <Button variant="outline-primary">Search</Button>
-             </Form>
-            <Nav className="mr-auto, icons">
-      <Nav.Link href="#location"><img src="https://img.icons8.com/ios/32/000000/compass.png" alt='compass'></img></Nav.Link>
-      <Nav.Link href="#heart"><img src="https://img.icons8.com/windows/32/000000/hearts.png" alt='heart'></img></Nav.Link>
-      <Nav.Link href="#user"><img src="https://img.icons8.com/ios/32/000000/gender-neutral-user.png" alt='user'></img></Nav.Link>
-    </Nav>
-    
-  </Navbar>
-            
+            <Navbar.Toggle className='mr-auto'  aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Form inline className='mr-auto'>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+                </Form>
+                <Nav className="mr-auto">
+                    <Nav.Link className='p-2 bd-highlight' href="#location"><img src="https://img.icons8.com/ios/32/000000/compass.png" alt='compass'></img></Nav.Link>
+                    <Nav.Link className='p-2 bd-highlight' href="#heart"><img src="https://img.icons8.com/windows/32/000000/hearts.png" alt='heart'></img></Nav.Link>
+                    <Nav.Link className='p-2 bd-highlight'href="#user"><img src="https://img.icons8.com/ios/32/000000/gender-neutral-user.png" alt='user'></img></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
         </div>
 
     );
 }
  
 export default SearchBar;
+
+
 
 
