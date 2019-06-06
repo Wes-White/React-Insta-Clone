bcac13a2df03fd888be1776a0bfb3ad15869cb5c
Wes-White <wesleyrwhite86@gmail.com>
2019-06-05 20:15:55 -0700

    added styling to SearchBar, import react bootstrap

--- instagram_app/src/Components/SearchBar/SearchBar.js
+++ instagram_app/src/Components/SearchBar/SearchBar.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './SearchBar.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
 function SearchBar(props) {
     return (  
         <div className='search-bar'>
-            <img className='logo' src="https://img.icons8.com/plasticine/100/000000/instagram.png" ßalt='logo'></img>
-            <input type="text" name="Search" placeholder='Search'></input>
-            <div className="searchBarIcons">
-                <img src="https://img.icons8.com/ios/32/000000/compass.png" alt='compass'></img>
-                <img src="https://img.icons8.com/windows/32/000000/hearts.png" alt='heart'></img>
-                <img src="https://img.icons8.com/ios/32/000000/gender-neutral-user.png" alt='user'></img>
-            </div>
+            <Navbar sticky='top' bg="light" variant="light">
+            <Navbar.Brand href="#home"><img className='logo' src="https://img.icons8.com/plasticine/100/000000/instagram.png" alt='logo'></img></Navbar.Brand>
+            <img className='writing' src={require('../../Images/instalogo.png' )}  alt='writingLogo'></img>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+             <Button variant="outline-primary">Search</Button>
+             </Form>
+            <Nav className="mr-auto, icons">
+      <Nav.Link href="#location"><img src="https://img.icons8.com/ios/32/000000/compass.png" alt='compass'></img></Nav.Link>
+      <Nav.Link href="#heart"><img src="https://img.icons8.com/windows/32/000000/hearts.png" alt='heart'></img></Nav.Link>
+      <Nav.Link href="#user"><img src="https://img.icons8.com/ios/32/000000/gender-neutral-user.png" alt='user'></img></Nav.Link>
+    </Nav>
+    
+  </Navbar>
             
         </div>
 
@@ -18,3 +30,5 @@ function SearchBar(props) {
 }
  
 export default SearchBar;
+
+
