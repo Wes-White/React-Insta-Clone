0adce432d50a4b7605bcf12221bfcf1ceecbbf14
Wes-White <wesleyrwhite86@gmail.com>
2019-06-05 21:21:58 -0700

    SearchBar styling completed

--- instagram_app/src/Components/SearchBar/SearchBar.js
+++ instagram_app/src/Components/SearchBar/SearchBar.js
@@ -10,15 +10,15 @@ function SearchBar(props) {
     return (  
         <div className='search-bar'>
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home"><img className='logo' src="https://img.icons8.com/plasticine/100/000000/instagram.png" alt='logo'></img></Navbar.Brand>
+            <Navbar.Brand  href="#home"><img className='logo' src="https://img.icons8.com/plasticine/100/000000/instagram.png" alt='logo'></img></Navbar.Brand>
             <img className='writing' src={require('../../Images/instalogo.png' )}  alt='writingLogo'></img>
-            <Navbar.Toggle className='mr-auto'  aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle  aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Form inline className='mr-auto'>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
+                <Form inline className='search , mr-auto'>
+                <FormControl type="text" placeholder="Search"  />
+                <Button variant="light">Search</Button>
                 </Form>
-                <Nav className="mr-auto">
+                <Nav>
                     <Nav.Link className='p-2 bd-highlight' href="#location"><img src="https://img.icons8.com/ios/32/000000/compass.png" alt='compass'></img></Nav.Link>
                     <Nav.Link className='p-2 bd-highlight' href="#heart"><img src="https://img.icons8.com/windows/32/000000/hearts.png" alt='heart'></img></Nav.Link>
                     <Nav.Link className='p-2 bd-highlight'href="#user"><img src="https://img.icons8.com/ios/32/000000/gender-neutral-user.png" alt='user'></img></Nav.Link>
