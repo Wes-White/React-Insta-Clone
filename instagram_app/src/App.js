e9f2982cf01a8d68a68a8938909b57a2318b102f
Wes-White <wesleyrwhite86@gmail.com>
2019-06-08 10:15:13 -0700

    started PostContainer

--- instagram_app/src/App.js
+++ instagram_app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import dummyData from '../src/dummy-data';
 import SearchBar from './Components/SearchBar/SearchBar';
-
+import PostContainer from './Components/PostContainer/PostContainer';
 
 class App extends React.Component {
   constructor(){
@@ -15,6 +15,7 @@ class App extends React.Component {
     return (
       <div className="App">
           <SearchBar data={this.state.data} />
+          <PostContainer data={this.state.data} />
       </div>
     );
   }
---
+++ instagram_app/src/Components/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PostContainer';
+
+function PostContainer(props) {
+    console.log(props)
+    return (  
+        <div> 
+        
+       </div>
+    );
+}
+ 
+export default PostContainer;
